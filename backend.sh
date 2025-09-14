3f9b51b39a241981c8a2833e4b5facd8b47522ea
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-14 22:06:45 +0530

    changes

---
+++ backend.sh
@@ -0,0 +1,66 @@
+#!?bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0]
+    then
+    echo -e "$R please run this script with root priveleges $N" &>>$LOG_FILE
+    exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2 is ...$R FAILED $N" &>>$LOG_FILE
+       exit 1
+    else
+       echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+    fi
+}
+
+echo "script started executing at: $(date)" | tee -a LOG_FILE
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable default nodejs"
+
+dnf module enable nodejs:20 -y &>>LOG_FILE
+VALIDATE $? "Enable nodejs:20 "
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Install nodejs"
+
+id expense &>>$LOG_FILE
+
+if [ $? -ne 0 ]
+then
+   echo -e "expense user not exists..$G created $N"
+   useradd expense &>>$LOG_FILE
+   VALIDATE $? "Creating expense user"
+else
+    echo -e "expense user already exists...$Y Skipping $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creating app folder"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend app code"
+
+cd /app
+
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting backend app code" 
---
+++ mysql.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMO.log"
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+       echo -e "$R please run this script with root priveleges $N" &>>$LOG_FILE
+       exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2 is....$R FAILED $N" .&>>$LOG_FILE
+    else
+       echo -e "$2 is....$G SUCCESS $N" .&>>$LOG_FILE
+    fi
+}
+
+echo "Script started executing at: $(date)" &>>$LOG_FILE #| tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MYSQL server"
+
+dnf enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MYSQL server"
+
+dnf start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MYSQL server"
+
+mysql -h mysql.awspractice.shop -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+   echo -e "MYSQL password is not setup, setting now ExpenseApp@1" &>>LOG_FILE
+   mysql_secure_installation --set-root-pass ExpenseApp@1
+   VALIDATE $? "Setting up root password" 
+else 
+   echo -e "MYSQL password is already set up....$Y skipping $N" &>>LOG_FILE
+fi
