a17884e7a777830f018b523c6bc8dc05c72ee93e
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-15 15:12:05 +0530

    changes

--- backend.sh
+++ backend.sh
@@ -64,22 +64,22 @@ rm -rf /app/* # remove the existing code
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
 
-# npm install &>>$LOG_FILE
-# cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service
+npm install &>>$LOG_FILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service
 
-# # load the data before running backend
+# load the data before running backend
 
-# dnf install mysql -y &>>$LOG_FILE
-# VALIDATE $? "Installing MySQL Client"
+dnf install mysql -y &>>$LOG_FILE 
+VALIDATE $? "Installing MySQL Client"
 
-# mysql -h mysql.awspractice.shop -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
-# VALIDATE $? "Schema loading"
+mysql -h mysql.awspractice.shop -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
 
-# systemctl daemon-reload &>>$LOG_FILE
-# VALIDATE $? "Daemon reload"
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
 
-# systemctl enable backend &>>$LOG_FILE
-# VALIDATE $? "Enabled backend"
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
 
-# systemctl restart backend &>>$LOG_FILE
-# VALIDATE $? "Restarted Backend"
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
