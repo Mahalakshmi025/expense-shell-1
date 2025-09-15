1a3467fbeb9853cc1ee21c9e4d43ee216a9ef171
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-15 15:03:06 +0530

    changes

--- backend.sh
+++ backend.sh
@@ -53,16 +53,16 @@ else
     echo -e "expense user already exists...$Y SKIPPING $N"
 fi
 
-# mkdir -p /app
-# VALIDATE $? "Creating /app folder"
+mkdir -p /app #here -p means if folder not exists creates otherwise not
+VALIDATE $? "Creating /app folder"
 
-# curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
-# VALIDATE $? "Downloading backend application code"
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
 
-# cd /app
+cd /app
 # rm -rf /app/* # remove the existing code
-# unzip /tmp/backend.zip &>>$LOG_FILE
-# VALIDATE $? "Extracting backend application code"
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting backend application code"
 
 # npm install &>>$LOG_FILE
 # cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service
