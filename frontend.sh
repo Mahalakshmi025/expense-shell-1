c71b6bb3432ef0f6b1ae6c823ecd22620e4e3a37
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-15 14:56:26 +0530

    changes

---
+++ frontend.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $? | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+       echo -e "$R Run the script with root privileges...$N"
+       exit 1
+    fi
+}
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+
+}
